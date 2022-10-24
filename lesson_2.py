4028de018dfb9d13e710e86c216876e098742a79
Evgeny <rudevgeny@gmail.com>
2022-10-24 15:29:18 +0300

    lesson2 complete

--- lesson_2.py
+++ lesson_2.py
@@ -12,7 +12,7 @@ def hashtag_serch(username, password, hashtag):
 
     try:
         browser.get('https://www.instagram.com')
-        time.sleep(random.randrange(3, 5))
+        time.sleep(random.randrange(1, 3))
 
         username_input = browser.find_element(By.NAME, 'username')
         username_input.clear()
@@ -31,10 +31,12 @@ def hashtag_serch(username, password, hashtag):
             browser.get(f'https://www.instagram.com/explore/tags/{hashtag}/')
             time.sleep(10)
 
+            for i in range(1, 4):
+                browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+                time.sleep(random.randrange(3, 5))
+
             hrefs = browser.find_elements(By.TAG_NAME, 'a')
-
             posts_urls = [item.get_attribute('href') for item in hrefs if '/p/' in item.get_attribute('href')]
-
             # posts_urls = []
             # for item in hrefs:
             #     href = item.get_attribute('href')
@@ -42,10 +44,15 @@ def hashtag_serch(username, password, hashtag):
             #         posts_urls.append(href)
             #         print(href)
 
-            for url in posts_urls[0:1]:
-                browser.get(url)
-                like_button = browser.find_element(By.XPATH, '//*[@id="mount_0_0_iw"]/div/div/div/div[1]/div/div/div/div[1]/section/main/div[1]/div[1]/article/div/div[2]/div/div[2]/section[1]/span[1]/button/div[2]/span/svg').click()
-                time.sleep(10)
+            for url in posts_urls:
+                try:
+                    browser.get(url)
+                    time.sleep(10)
+                    like_button = browser.find_element(By.XPATH, '/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/section/main/div[1]/div[1]/article/div/div[2]/div/div[2]/section[1]/span[1]/button').click()
+                    time.sleep(random.randrange(80, 120))
+                except Exception as ex:
+                    print(ex)
+
 
             browser.close()
             browser.quit()
